b1c747f16e9ad9c87661f0277dc45b74af5a3b51
Kisore4399 <3580309627@qq.com>
2022-10-15 17:31:40 +0800

    modified20221015129

---
+++ CMakeFiles/proj02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj02.dir/project02.cpp.o"
+  "proj02"
+  "proj02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
